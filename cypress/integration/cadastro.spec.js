475a0a8ce73745f2b7725ee5e6970a689a4e2ba4
Adriana Monteiro <adriianamonteiiro@gmail.com>
2022-03-22 20:39:49 -0300

    Alteração na arquitetura dos dados de teste

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -3,54 +3,26 @@ import signup from '../pages/SignupPage'
 
 describe('Cadastro', () => {
 
-    it('Usuário deve se tornar um entregador', () => {
+    beforeEach(function () {
+        cy.fixture('entregador').then((e) => {
+            this.entregador = e
+        })
+    })
 
-        var entregador = {
-            nome: 'Test',
-            cpf: '00000014141',
-            email: 'test@gmail.com',
-            whatsapp: '11999999999',
-            endereco: {
-                cep: '04534011',
-                rua: 'Rua Joaquim Floriano',
-                numero: '1000',
-                complemento: 'apto 84',
-                bairro: 'Itaim Bibi',
-                cidade_uf: 'São Paulo/SP'
-            },
-            metodo_entrega: 'Moto',
-            cnh: 'cnh-digital.jpg'
-        }
+    it('Usuário deve se tornar um entregador', function () {
 
         signup.go()
-        signup.fillForm(entregador)
+        signup.fillForm(this.entregador.signup)
         signup.submit()
         const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
         signup.modalContentShouldBe(expectedMessage)
 
     })
 
-    it('CPF inválido', () => {
-
-        var entregador = {
-            nome: 'Test',
-            cpf: '00000014141AA',
-            email: 'test@gmail.com',
-            whatsapp: '11999999999',
-            endereco: {
-                cep: '04534011',
-                rua: 'Rua Joaquim Floriano',
-                numero: '1000',
-                complemento: 'apto 84',
-                bairro: 'Itaim Bibi',
-                cidade_uf: 'São Paulo/SP'
-            },
-            metodo_entrega: 'Moto',
-            cnh: 'cnh-digital.jpg'
-        }
+    it('CPF inválido', function () {
 
         signup.go()
-        signup.fillForm(entregador)
+        signup.fillForm(this.entregador.cpf_inv)
         signup.submit()
         signup.alertMessageShouldBe('Oops! CPF inválido')
 
